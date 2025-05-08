bd91553c934d8240f7bee69bfb9f231173d98308
denny0810 <59128785+denny0810@users.noreply.github.com>
2025-05-08 16:11:00 +0800

    Update chat.ts

--- src/api/routes/chat.ts
+++ src/api/routes/chat.ts
@@ -5,6 +5,8 @@ import Response from "@/lib/response/Response.ts";
 import chat from "@/api/controllers/chat.ts";
 import logger from "@/lib/logger.ts";
 
+// 容器环境变量 `QWEN_CHAT_AUTHORIZATION` 
+const CHAT_AUTHORIZATION = process.env.CHAT_AUTHORIZATION;
 export default {
   prefix: "/v1/chat",
 
@@ -14,6 +16,12 @@ export default {
         .validate('body.conversation_id', v => _.isUndefined(v) || _.isString(v))
         .validate("body.messages", _.isArray)
         .validate("headers.authorization", _.isString);
+
+     // 如果环境变量没有token则读取请求中的
+     if (CHAT_AUTHORIZATION) {
+         request.headers.authorization = "Bearer " + CHAT_AUTHORIZATION;
+      }      
+      
       // refresh_token切分
       const tokens = chat.tokenSplit(request.headers.authorization);
       // 随机挑选一个refresh_token
